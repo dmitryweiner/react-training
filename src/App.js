deee2c1cf24ad8a6a423123cf582ad860ac59c0d
dmitryweiner <dmitry.weiner@gmail.com>
2020-03-18 19:34:53 +0700

    Added chat code to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const apiUrl = "http://localhost:3001";
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      message: "",
+      nick: ""
+    };
+
+    this.receiveMessages = this.receiveMessages.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
+  }
+
+  componentDidMount() {
+    setInterval(this.receiveMessages, 1000);
+  }
+
+  receiveMessages() {
+    const http = new XMLHttpRequest();
+    http.open("GET", apiUrl, true);
+    http.onreadystatechange = () => {
+      if (http.readyState === 4 && http.status === 200) {
+        this.updateMessages(http.responseText);
+      }
+    };
+    http.send();
+  }
+
+  sendMessage() {
+    const http = new XMLHttpRequest();
+    http.open("POST", apiUrl, true);
+    //http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.onreadystatechange = () => {
+      if (http.readyState === 4 && http.status === 200) {
+        this.updateMessages(http.responseText);
+      }
+    };
+    http.send(JSON.stringify({
+      message: this.state.message,
+      nick: this.state.nick
+    }));
+    this.setState({message: ""});
+  }
+
+  updateMessages(messagesAsText) {
+    const messages = JSON.parse(messagesAsText);
+    this.setState({ messages });
+  }
+
+  render() {
+      const {messages, message, nick} = this.state;
+
+      return (
+        <div className="App">
+          <ul>
+            {messages.map((item, index) =>
+              <li key={index}>
+                <strong>{item.nick}:</strong> {item.message}
+              </li>
+            )}
+          </ul>
+          <form>
+            <input
+                value={nick}
+                type="text"
+                onChange={(event) => this.setState({nick: event.target.value})}/>
+            <br/>
+            <textarea
+                value={message}
+                onChange={(event) => this.setState({message: event.target.value})}>
+            </textarea>
+            <br/>
+            <input type="button" value="Send" onClick={this.sendMessage}/>
+          </form>
+        </div>
+        );
+    }
 }
 
 export default App;
