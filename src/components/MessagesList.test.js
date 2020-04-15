39af214fa2aa9966cb64949a2ba092e9cd8624bc
dmitryweiner <dmitry.weiner@gmail.com>
2020-04-15 12:42:09 +0700

    Delete redundant import

--- src/components/MessagesList.test.js
+++ src/components/MessagesList.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { createStore } from 'redux'
 // We're using our own custom render function and not RTL's render
 // our custom utils also re-export everything from RTL
 // so we can import fireEvent and screen here as well
