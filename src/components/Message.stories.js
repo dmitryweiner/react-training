fc02e6c9be2fc7538cb7c9e249fb3832926f57e3
dmitryweiner <dmitry.weiner@gmail.com>
2020-05-01 13:20:32 +0700

    Added knobs to Message storybook

--- src/components/Message.stories.js
+++ src/components/Message.stories.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Message from './Message';
+import { withKnobs, text, boolean, number } from "@storybook/addon-knobs";
 
 export default {
     title: 'Message',
     component: Message,
+    decorators: [withKnobs]
 };
 
 export const ShowText = () => {
     const message = {
-        nick: "Nobody",
-        message: "Hello everybody!"
+        nick: text("Nickname", "Nobody"),
+        message: text("Message", "Hello everybody!")
     };
     return <Message message={message}/>;
 };
