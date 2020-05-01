e803e3b33df88855ecb0fc045fe468b1433d0d6d
dmitryweiner <dmitry.weiner@gmail.com>
2020-05-01 16:18:42 +0700

    Added Loki

---
+++ storybook-static/precache-manifest.3b00254c7f8ed5b9c7c4756e74995dcd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ea91fcd0bc63ae4bbebc5947cccc97",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.0b13f07f7dffe306203e.bundle.js"
+  },
+  {
+    "url": "runtime~main.0b13f07f7dffe306203e.bundle.js"
+  },
+  {
+    "url": "vendors~main.0b13f07f7dffe306203e.bundle.js"
+  },
+  {
+    "url": "vendors~main.0b13f07f7dffe306203e.bundle.js.LICENSE.txt"
+  }
+]);
